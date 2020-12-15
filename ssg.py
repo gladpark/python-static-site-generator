dfe3bb2fb34e26bc7aaa57b754d80c0f50d1b342
gladpark <accounts@gladpark.net>
2020-12-15 13:50:51 +1000

    finish step 2

--- ssg.py
+++ ssg.py
@@ -3,6 +3,13 @@ from ssg.site import Site
 
 
 def main(source='content', dest='dest'):
-    pass
+    config = {'source': source, 'dest': dest}
+    site = Site(**config)
+    site.build()
+
+
+typer.run(main)
+
+
 
 
